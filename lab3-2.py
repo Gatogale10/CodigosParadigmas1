a56b4f884c7666758ee136f3a018809be96ecef1
Gatogale10 <109935791+Gatogale10@users.noreply.github.com>
2023-01-22 13:50:24 -0600

    Update lab3-2.py

--- lab3-2.py
+++ lab3-2.py
@@ -6,8 +6,8 @@ def saludo():
     #=======================================
     # Documentacion rapida de la funcion
     #=======================================
-    """Esta funcion saluda"""
-    print('¡Quiuboles! , ¿como estas?´)
+   # """Esta funcion saluda"""
+    print('¡Quiuboles! , ¿como estas?')
     
 #=========================
 # Llamado de la funcion
@@ -34,7 +34,7 @@ print(salida)
 #==================================
 
 def salu2(nombre):
-    """Esta funcion te saluda por tu nombre"""
+    #"""Esta funcion te saluda por tu nombre"""
     print("¡Que onda ese",nombre,"!")
 salu2("Julian")
 salu2("Angel")
@@ -45,12 +45,13 @@ salu2("Angel")
 #============================================
 
 def saludos(nombre:str):
-    """ Esta funcion te saluda por tu nombre estrictamente"""
+    #""" Esta funcion te saluda por tu nombre estrictamente"""
     print("¡Que onda ese",nombre,"!")
-saludos("Julian)
+
+saludos("Julian")
 a = 123
 print(type(a))
-saludos(a)
+
 
 
 #=======================================
@@ -58,7 +59,7 @@ saludos(a)
 #=======================================
 
 def saludos_multiples(nombre1,nombre2,nombre3):
-    """Esta funcion saluda a 3 personas al mismo tiempo"""
+   # """Esta funcion saluda a 3 personas al mismo tiempo"""
     print("Hola", nombre1,",",nombre2,"y",nombre3)
 saludos_multiples("Hugo","Paco","Luis")
 
@@ -67,7 +68,7 @@ saludos_multiples("Hugo","Paco","Luis")
 #=======================================================
 
 def muchos_saludos(*nombres):
-    """ Esta funcion saluda a todos los que quieras"""
+    # """ Esta funcion saluda a todos los que quieras"""
     
     i=0
     #=======================================
@@ -75,24 +76,24 @@ def muchos_saludos(*nombres):
     #=======================================
     
     print("Hola",end="")
-    while len(nombres)>i 
+    while len(nombres)>i:
       # Ultimo nombre 
-      if (i==len(nombres)-1:
+      if (i==len(nombres)-1):
         print(nombres[i])
       else:
-      #Cualquier otro nombre
-        print(nombre[1], end=",")        
+        #Cualquier otro nombre
+        print(nombres[1], end=",")        
       i+=1
        
 muchos_saludos("Bosco","Angel","Davidad", "Tamara","Mili","Edwin","Lev","Luis","Abigail")
-def greet(firstname, lastname);
+def greet(firstname, lastname):
     print('Hello', firstname, lastname)
 
 #=================================================
 #  Llamar a la funcion con argumentos en desorden 
 #=================================================     
 
-greet=(lastname='Jobs', firstname='Steve')# Se puede especificar las variables en desorden
+greet( lastname='Jobs', firstname='Steve')# Se puede especificar las variables en desorden
 
 #====================================
 # Funcion con argumentos escondidos**
@@ -103,9 +104,9 @@ def greet(**person):
     #======================================================
     print('Hello',person['firstname'], person['lastname'])
     
-great(firstname='Steve', lastname= 'Jobs')
-great(lastname='Jobs', firstname='Steve')
-great(firstname='Bill', lastname='Gates', age=55) # Se puede pasar mas parametros de los necesarios 
+greet(firstname='Steve', lastname= 'Jobs')
+greet(lastname='Jobs', firstname='Steve')
+greet(firstname='Bill', lastname='Gates', age=55) # Se puede pasar mas parametros de los necesarios 
 
 #===================================
 # Funcion con valores por defecto 
